41398da18932a3873ed6a4d38ae11b9c71a1d6b7
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-03-27 22:55:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+//array functions
+ let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+};
+ 
+//unshift
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+};
+
+
+//.pop method
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+};
+
+
+//.shift method
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+};
+
+//spread operator
+ function appendCat(name){
+  return [...cats,name];
+};
+
+function prependCat(name){
+  return [name,...cats];
+};
+
+function removeLastCat(){
+  return ["Milo", "Otis"];
+};
+
+function removeFirstCat(){
+   return ["Otis", "Garfield"]
+};
